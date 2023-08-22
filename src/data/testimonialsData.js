7377bcfaafc5afa571166d8ed32c4418a80d09a7
Regina <80333829+Regina-alt@users.noreply.github.com>
2023-08-22 13:41:48 +0200

    Update testimonialsData.js

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,20 +1,20 @@
 
 export const testimonialsData = [
   {
-    image: "/img/t-image1.png",
+    image: "img/t-image1.png",
     review:
       "I made the right choice by choosing the Fitclub and I already achieved my ideal body!",
     name: 'MATHEW HENDRICKSON',
     status : 'ENTREPRENEUR'
   },
   {
-    image: "/img/t-image2.jpg",
+    image: "img/t-image2.jpg",
     review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
     name: 'JOHN KEVIN',
     status: 'COACH'
   },
   {
-    image : "/img/t-image3.jpg",
+    image : "img/t-image3.jpg",
     review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
     name: 'FRANKLIN',
     status: "CUSTOMER"
